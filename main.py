a8fe84f55894898f4bf20d20183515bac915fab0
Rohith Suresh <53375483+Xylegen@users.noreply.github.com>
2021-09-16 05:55:58 +0000

    Security Changes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import discord
 import requests
 import json
 
-
 def get_news():
   query={
     "sources": "Axios",
@@ -32,13 +31,9 @@ async def on_message(message):
   if message.author==client.user:
     return
   if message.content.startswith("$news"):
-   # quote=get_quote()
     results,urls=get_news()
     for i in range(5):
       await message.channel.send(f"{i+1}. {results[i]}\n")
       await message.channel.send(f"{urls[i]}\n")
-    #mention=message.author.mention
-    
-    #await message.channel.send(f"{mention}, You got this homes!")
 
 client.run(TOKEN)
