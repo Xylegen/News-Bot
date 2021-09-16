73a2d9eecc91b4d931d0a71df06862e0bd4e1c64
Rohith Suresh <53375483+Xylegen@users.noreply.github.com>
2021-09-16 05:42:12 +0000

    Converted bot to News Bot that print

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import os
-import discord 
+import discord
+import requests
+import json
 
-#TOKEN=os.environ['DISC_TOKEN']
+#def get_quote():
+ #response=requests.get("https://zenquotes.io/api/random")
+ # json_data=json.loads(response.text)
+ #quote=json_data[0]['q']+ " -" + json_data[0]['a']
+  #return(quote)
+
+def get_news():
+  query={
+    "sources": "Axios",
+    "apiKey": "920ba8c3f59e4e369c6f12e914800c6d"
+  }
+  response=requests.get("https://newsapi.org/v2/top-headlines", params=query)
+  bbc_page=response.json()
+  article=bbc_page["articles"]
+  titles=[]
+  urls=[]
+  for ar in article:
+    titles.append(ar["title"])
+  for ra in article:
+    urls.append(ar["url"])
+
+  return(titles,urls)
+
+
+
 client=discord.Client()
 TOKEN=os.environ['DISC_TOKEN']
 @client.event
@@ -12,8 +38,14 @@ async def on_ready():
 async def on_message(message):
   if message.author==client.user:
     return
-  
-  if message.content.startswith('$hello'):
-    await message.channel.send('WHATUP!')
+  if message.content.startswith("$news"):
+   # quote=get_quote()
+    results,urls=get_news()
+    for i in range(5):
+      await message.channel.send(f"{i+1}. {results[i]}\n")
+      await message.channel.send(f"{urls[i]}\n")
+    #mention=message.author.mention
+    
+    #await message.channel.send(f"{mention}, You got this homes!")
 
 client.run(TOKEN)
